478f9e391c1bf42d16d2ef55628290c78d9879ae
Brian Lawi Ochiel <lawioochiel@gmail.com>
2023-10-03 22:36:17 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function saturdayFun ( activity ="roller-skate" ){
+     return `This Saturday, I want to ${activity}!`
+    }
+    
+    saturdayFun ("bathe my dog");
+
+    const mondayWork = function( task = 'go to the office'){
+        return `This Monday, I will ${task}.`
+    }
+    mondayWork ("work from home");
+
+    function wrapAdjective( sym = "*"){
+        return function(what = "special"){
+            return `You are ${sym}${what}${sym}!`
+        }
+    }
+    
+    wrapAdjective("%")("a dedicated programmer")
